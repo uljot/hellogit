639ed3b7405ca65e1b1cfdde5d2d6911df6ae51d
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2017-10-17 09:32:21 +0200

    update list to display all xml files

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -4,13 +4,34 @@ class ListController
 {
     public function listAction($request)
     {
-        $path = __ROOTDIR__.'var/students-list.xml';
+        $list = [];
 
-        $students = simplexml_load_file($path);
-
+        foreach ($this->getStudentsFiles() as $file) {
+            $students = simplexml_load_file($file->getPathname());
+            $list = $list + $students->xpath('//student');
+        }
+        
         return array(
             'template' => 'list',
-            'data' => $students->student
+            'data' => $list
         );
+    }
+
+    private function endsWith($haystack, $needle) {
+	    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
+	}
+
+    private function getStudentsFiles() 
+    {
+        $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__ROOTDIR__.'var/'));
+
+        $files = [];
+        foreach($objects as $name => $object){
+            if ($this->endsWith($name, '.xml')) {
+                $files[] = $object;
+            }
+        }
+
+        return $files;
     }
 }
